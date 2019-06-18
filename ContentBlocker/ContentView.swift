3503283a4d93370c445e242f103aa02334c285b0
cpsdqs <cpsdqs@gmail.com>
2019-06-18 22:08:11 +0200

    Initial Commit

---
+++ ContentBlocker/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  ContentBlocker
+//
+//  Created by cpsdqs on 2019-06-18.
+//  Copyright © 2019 cpsdqs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
